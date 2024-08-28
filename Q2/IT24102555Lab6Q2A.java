e9355416348778218bdab7317270aacba74d2302
IT24102555 <it24102555@my.sliit.lk>
2024-08-28 09:55:46 +0530

    Question2A

---
+++ Q2/IT24102555Lab6Q2A.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+  public class IT24102555Lab6Q2A {
+    public static void main(String[] args){
+     int num=1;
+while(num<=10){
+     System.out.print(num + " ");
+     num=num+1;
+  }
+ }
+}
