d0a2bab5aa7f3d19b5eb72facff9b532c146f41a
IT24102555 <it24102555@my.sliit.lk>
2024-09-17 17:45:42 +0530

    Add files via upload

---
+++ Q3/IT24102555Lab6Q3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class IT24102555Lab6Q3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int count = 0;
+        int sumOfSquares = 0;
+        
+
+        System.out.println("Enter positive integers (terminate with -99):");
+
+        while (true) {
+            System.out.print("Enter a number :");
+            int num = input.nextInt();
+
+            
+            if (num == -99) {
+                break;
+            }
+
+            if (num < 0) {
+                System.out.println("Please enter a positive integer or -99 to terminate:");
+                continue;
+            }
+
+
+           sumOfSquares =  sumOfSquares + num * num;
+            count++;
+        }
+
+            double rootMeanSquare = Math.sqrt(sumOfSquares / count);
+            System.out.println("The Root Mean Square of the entered numbers: " + rootMeanSquare);
+       
+
+    }
+}
